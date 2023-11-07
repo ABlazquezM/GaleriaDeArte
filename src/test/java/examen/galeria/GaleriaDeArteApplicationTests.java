5e5927b863af36944df5f698e9aacc7450ef5d0c
ABlazquezM <andrea.blazquez.martin@gmail.com>
2023-11-07 20:54:52 +0100

    Preparación de Remoto

---
+++ src/test/java/examen/galeria/GaleriaDeArteApplicationTests.java
@@ -0,0 +1,13 @@
+package examen.galeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaleriaDeArteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
